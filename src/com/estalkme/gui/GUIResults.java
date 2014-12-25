a7f2a252bf20911cc665338d763c7d8512674828
Joey Bronner <joeybronner@gmail.com>
2014-12-25 22:19:03 +0100

    User img for identity card ; Ignore build directory

--- src/com/estalkme/gui/GUIResults.java
+++ src/com/estalkme/gui/GUIResults.java
@@ -2,11 +2,23 @@ package com.estalkme.gui;
 
 import java.awt.BorderLayout;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JMenuBar;
+
 import java.awt.Color;
+
+import javax.swing.JLabel;
+
+import java.awt.Font;
+
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import com.jgoodies.forms.layout.RowSpec;
 
 public class GUIResults extends JFrame {
 
@@ -32,5 +44,36 @@ public class GUIResults extends JFrame {
 		window.setBorder(new EmptyBorder(5, 5, 5, 5));
 		window.setLayout(new BorderLayout(0, 0));
 		f.setContentPane(window);
+		
+		JPanel left = new JPanel();
+		window.add(left, BorderLayout.WEST);
+		left.setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.LABEL_COMPONENT_GAP_COLSPEC,
+				ColumnSpec.decode("185px"),},
+			new RowSpec[] {
+				FormFactory.LINE_GAP_ROWSPEC,
+				RowSpec.decode("17px"),
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,}));
+		
+		JLabel lblCarteDidentitNumrique = new JLabel("Carte d'identit\u00E9 num\u00E9rique");
+		lblCarteDidentitNumrique.setFont(new Font("Tahoma", Font.BOLD, 14));
+		left.add(lblCarteDidentitNumrique, "2, 2, left, top");
+		
+		JLabel imgUser = new JLabel("");
+		imgUser.setIcon(new ImageIcon(ImageIO.read(getClass().getResource("img/user.png"))));
+		left.add(imgUser, "2, 4, center, default");
+		
+		JPanel header = new JPanel();
+		window.add(header, BorderLayout.NORTH);
+		
+		JPanel footer = new JPanel();
+		window.add(footer, BorderLayout.SOUTH);
+		
+		JPanel right = new JPanel();
+		window.add(right, BorderLayout.EAST);
+		
+		JPanel body = new JPanel();
+		window.add(body, BorderLayout.CENTER);
 	}
 }
